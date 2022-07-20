02faee4599074572245ee73e707576d045c99d0a
bluepost59 <kaput.kat@gmail.com>
2022-07-20 18:35:24 +0900

    バイトは読めた（パースが大変）

---
+++ read_float_array.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    with open("float_array.dat","rb") as f:
+        x = f.read()
+    print(x)
+
+if __name__=="__main__":
+    main()
--- test_float_array.f90
+++ test_float_array.f90
@@ -1,5 +1,21 @@
 program test_float_array
     implicit none
-    write(6,*) "hello"
+
+    double precision,dimension(100) :: x
+
+    integer :: output_file_unit = 100
+
+    ! 乱数生成
+    ! https://www.nag-j.co.jp/fortran/tips/tips_RandomNumberInFortran.html
+
+    open(output_file_unit, &
+        file="float_array.dat", & 
+        form="unformatted", & 
+        status="replace")
+
+    call random_number(x)
+    ! write(6,*) x
+    write(output_file_unit) x
+    close(output_file_unit)
 
 end program test_float_array
